74959e493508b2de65527a6b4dcb92131a1009ea
ArtiolaDomiCaka <artioladomi@hotmail.com>
2021-11-17 09:36:58 +0100

    Fix some problems

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
+ 
+ let age = Number(prompt("What's your age?"));
+ let distance = Number(prompt("How far are you travelling?"));
 
-const priceForKm= 0.21
+ 
+const priceForKm= 0.21;
 let basicPrice= priceForKm * distance;
-let totalPrice= Number;
-let age = Number(prompt("What's your age?"));
-let distance = Number(prompt("How far are you travelling?"));
+
+
+
 
 if(age<18){
-    totalPrice=(totalPrice * 0.8);
-    document.write("You need to pay" + totalPrice)
+    discountedPrice =(basicPrice * 0.8);
+    document.write("You need to pay" + "£" + discountedPrice.toFixed(2));
 }else if (age>65){
-    totalPrice=(totalPrice * 0.6);
-    document.write("You need to pay" + totalPrice)
+    discountedPrice =(basicPrice * 0.6);
+    document.write("You need to pay" + "£" + discountedPrice.toFixed(2));
 }else {
-    totalPrice  * distance;
-    document.write("You need to pay" + totalPrice)
-}
+    basicPrice  * distance;
+    document.write("You need to pay"  + "£"+ basicPrice.teFixed(2));
+};
 
 
 
